919140c029d4213bc7ff7b6219cfb9a85237048a
pding <395719317@qq.com>
2019-03-20 14:38:28 +0800

    添加开发文件

---
+++ wepy-mall-master/src/utils/dataJson/discoverList.js
@@ -0,0 +1,83 @@
+var data={
+  "reason": "",
+  "code": "0",
+  "list": [
+    {
+      "code": "022",
+      "logo": "http://yzb.nitago.com/upload/2018/10/19/1539918866.jpg",
+      "id": "2c9257a16126d14701612b52808100d6",
+      "attrs": [
+        {
+          "attrValList": [
+            {
+              "attrName": "品牌",
+              "attrNameId": 215,
+              "attrVal": "素洁",
+              "id": 558
+            }
+          ],
+          "attrName": {
+            "attrName": "品牌",
+            "categoryCode": "022",
+            "id": 215
+          }
+        }/*,
+        {
+          "attrValList": [
+            {
+              "attrName": "年份季节",
+              "attrNameId": 216,
+              "attrVal": "2018秋季",
+              "id": 559
+            }
+          ],
+          "attrName": {
+            "attrName": "年份季节",
+            "categoryCode": "022",
+            "id": 216
+          }
+        }*/
+      ]
+    },
+    {
+      "code": "021",
+      "logo": "http://sujiefs.com/upload/images/20180319/201803191401583397599.jpg",
+      "id": "2c9257a160e0cb1c0160ee3562e0024e",
+      "attrs": [
+        {
+          "attrValList": [
+            {
+              "attrName": "品牌",
+              "attrNameId": 211,
+              "attrVal": "素洁",
+              "id": 554
+            }
+          ],
+          "attrName": {
+            "attrName": "品牌",
+            "categoryCode": "021",
+            "id": 211
+          }
+        }/*,
+        {
+          "attrValList": [
+            {
+              "attrName": "年份季节",
+              "attrNameId": 212,
+              "attrVal": "2018全新升级版",
+              "id": 555
+            }
+          ],
+          "attrName": {
+            "attrName": "年份季节",
+            "categoryCode": "021",
+            "id": 212
+          }
+        }*/
+      ]
+    }
+  ]
+};
+module.exports = {
+  data
+};
